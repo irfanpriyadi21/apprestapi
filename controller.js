2ff30099d2decd737b3261483844f3932aef876c
Irfan Priyadi Nurfauzi <irfanpnf@gmail.com>
2021-01-18 22:00:52 +0700

    get data Mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok('Api running', res);
 };
+
+exports.getMahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa',function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+}
--- router.js
+++ router.js
@@ -4,4 +4,6 @@ module.exports = function(app){
     var json = require('./controller');
 
     app.route('/').get(json.index);
+
+    app.route('/getMahasiswa').get(json.getMahasiswa);
 }
